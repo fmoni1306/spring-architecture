58e473a9b8f591f866d0a5cb08c04f018c91ef56
Yoon <fmoni1306@gmail.com>
2024-09-05 00:25:33 +0900

    init

---
+++ src/main/java/com/study/springarchitecture/SpringArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.study.springarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springarchitecture/SpringArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
